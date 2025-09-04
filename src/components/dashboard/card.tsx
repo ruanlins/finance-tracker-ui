9c4cce21bac5ee03e912237f9413c9155bbef04e
Ruan Lins <ruan.lins@smartbreeder.com.br>
2025-09-04 08:56:31 -0300

    Fixed dashboard layout

--- src/components/dashboard/card.tsx
+++ src/components/dashboard/card.tsx
@@ -1,15 +1,21 @@
-import { ArrowUpRight } from "lucide-react";
+import { ArrowDownRight, ArrowUpRight } from "lucide-react";
 import { Card, CardHeader, CardTitle, CardContent } from "../ui/card";
 
-export function DashboardCard() {
+interface DashboardCardProps {
+  title: string
+  value: number
+  comparison: number
+}
+
+export function DashboardCard({ title, value, comparison }: DashboardCardProps) {
   return (
-    <Card className="max-w-74 gap-0.5">
+    <Card className="gap-0.5">
       <CardHeader>
-        <CardTitle className="text-lg font-medium">Carteira</CardTitle>
+        <CardTitle className="text-lg font-medium">{title}</CardTitle>
       </CardHeader>
       <CardContent className="flex items-end justify-between">
-        <p className="text-primary text-3xl font-bold">R$1500,00</p>
-        <span className="flex font-medium items-center border-[1px] py-1 px-2 rounded-lg"><ArrowUpRight size={22} className="text-primary" />5%</span>
+        <p className="text-primary text-3xl font-bold">{value.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}</p>
+        <span className="flex items-center border-[1px] py-1 px-2 rounded-lg">{comparison > 0 ? <ArrowUpRight size={22} className="text-primary" /> : <ArrowDownRight size={22} className="text-destructive" />}{comparison}%</span>
       </CardContent>
     </Card>
   );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,12 +8,14 @@ export const Route = createFileRoute("/")({
   component: RouteComponent,
 });
 
+const data = [{ title: 'Carteira', value: 1500, comparison: 5 }, { title: 'Gastos', value: 2300, comparison: -13 }]
+
 function RouteComponent() {
   const [period, setPeriod] = React.useState<string>("month");
   const [timeRange, setTimeRange] = React.useState<DateRange | undefined>();
   return (
     <div className="m-auto w-4/6">
-      <div className="flex justify-between">
+      <div className="flex justify-between my-12">
         <h1 className="text-3xl font-semibold">Olá, Usuário!</h1>
         <Filters
           period={period}
@@ -22,8 +24,10 @@ function RouteComponent() {
           setTimeRange={setTimeRange}
         />
       </div>
-      <div>
-        <DashboardCard />
+      <div className="grid grid-cols-3 gap-20 my-12">
+        <DashboardCard title={data[0].title} value={data[0].value} comparison={data[0].comparison} />
+        <DashboardCard title={data[0].title} value={data[0].value} comparison={data[0].comparison} />
+        <DashboardCard title={data[1].title} value={data[1].value} comparison={data[1].comparison} />
       </div>
     </div>
   );
